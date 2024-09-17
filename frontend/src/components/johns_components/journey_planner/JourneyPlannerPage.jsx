6ee5d4cc6fa39bc3b1b6cad7fc2eeaaeb65784b1
Tajul Mazlan <tajulm@missionreadyhq.com>
2024-09-17 13:18:12 +1200

    added search input component for journey planner. still trying to figure out how to use google map geocoding api

--- frontend/src/components/johns_components/journey_planner/JourneyPlannerPage.jsx
+++ frontend/src/components/johns_components/journey_planner/JourneyPlannerPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import JourneyPlannerLanding from "./JourneyPlannerLanding";
+import SearchInput from "./SearchInput";
 
 function JourneyPlannerPage() {
   return (
     <div>
       <JourneyPlannerLanding />
+      <SearchInput/>
     </div>
   );
 }
