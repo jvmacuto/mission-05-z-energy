4c3452da98f3d131877a920a0a08bcab3b3bac9d
Tajul Mazlan <tajulm@missionreadyhq.com>
2024-09-17 17:38:18 +1200

    have created the Map component to frontend but could not fetch the backend

--- frontend/src/components/johns_components/journey_planner/JourneyPlannerPage.jsx
+++ frontend/src/components/johns_components/journey_planner/JourneyPlannerPage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import JourneyPlannerLanding from "./JourneyPlannerLanding";
 import SearchInput from "./SearchInput";
+import MapComponent from "./MapComponent";
 
 function JourneyPlannerPage() {
   return (
     <div>
       <JourneyPlannerLanding />
       <SearchInput/>
+      <MapComponent/>
     </div>
   );
 }
