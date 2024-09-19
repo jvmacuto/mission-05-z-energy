73e281896e01a4bda06aa8d509693884f664a98b
John Vincent <macutojohnvincent@gmail.com>
2024-09-20 09:35:33 +1200

    fixed bugs

--- frontend/src/components/javans_components/store_information/FuelPrices.jsx
+++ frontend/src/components/javans_components/store_information/FuelPrices.jsx
@@ -2,36 +2,27 @@ import React from "react";
 
 const FuelPrices = ({ prices }) => {
   return (
-    <div className="fuel-prices">
-      <ul>
-        <li>
-          <div className="fuel-type">
+    <div className="fuel-prices-container">
+      <ul className="fuel-prices-list">
+        <li className="fuel-prices-item">
+          <div className="fuel-prices-type">
             <span>Premium</span>
-            <span className="price">${prices.premium} / L</span>
+            <span className="fuel-prices-price">${prices.premium} / L</span>
           </div>
         </li>
-        <li>
-          <div className="fuel-type">
+        <li className="fuel-prices-item">
+          <div className="fuel-prices-type">
             <span>Unleaded</span>
-            <span className="price">${prices.unleaded} / L</span>
+            <span className="fuel-prices-price">${prices.unleaded} / L</span>
           </div>
         </li>
-        <li>
-          <div className="fuel-type">
+        <li className="fuel-prices-item">
+          <div className="fuel-prices-type">
             <span>Diesel</span>
-            <span className="price">${prices.diesel} / L</span>
+            <span className="fuel-prices-price">${prices.diesel} / L</span>
           </div>
         </li>
       </ul>
-
-      {/* New section under the fuel prices */}
-      <div className="fuel-extra">
-        <h3>Payment</h3>
-        <div className="fuel-payment-methods">
-          <span>Pay in app</span>
-          <span>Pay at pump</span>
-        </div>
-      </div>
     </div>
   );
 };
